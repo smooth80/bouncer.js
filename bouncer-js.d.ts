9832435b647713694ccc3c54d710161b5a36757c
Jacek Pietal <prozi85@gmail.com>
2021-04-15 01:52:59 +0200

    Update bouncer-js.d.ts

--- bouncer-js.d.ts
+++ bouncer-js.d.ts
@@ -1,5 +1,13 @@
 declare module '@jacekpietal/bouncer.js/build/plugins/chat/ng/chat.service' {
+  class UWebSocket extends WebSocket {
+    emitEvent(event: string, data: any): void;
+    emit(data: any): void;
+    on(event: string, cb: (...args: any[]) => void): void;
+  }
+
   export class ChatService {
+    socket: UWebSocket;
     constructor(window: Window)
+    connect(address?: string): void;
   }
 }
