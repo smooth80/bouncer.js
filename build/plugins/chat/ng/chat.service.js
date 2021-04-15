d4592410d4959e5702b8600d6000d7e976685237
Jacek Jan Pietal <jacekjanpietal@pop-os.localdomain>
2021-04-15 02:17:26 +0200

    feat: new build

--- build/plugins/chat/ng/chat.service.js
+++ build/plugins/chat/ng/chat.service.js
@@ -1,67 +1,47 @@
-import { __awaiter, __decorate, __metadata, __param } from 'tslib'
-import { Inject, Injectable } from '@angular/core'
-import UWebSocket from '../../../client'
-import { Subject } from 'rxjs'
+import { __awaiter, __decorate, __metadata, __param } from "tslib";
+import { Inject, Injectable } from '@angular/core';
+import UWebSocket from '../../../client';
+import { Subject } from 'rxjs';
 let ChatService = class ChatService {
-  constructor(window) {
-    this.window = window
-    this.topic = 'chat'
-    this.joinEvent = '/join'
-    this.message = new Subject()
-    this.messages = []
-    this.messagesLimit = 1000
-    this.serverUrl = this.window.location.origin.replace(/^http/, 'ws')
-    this.initialize()
-  }
-  send(event, data, onError = (e) => console.warn(e)) {
-    if (!this.socket) {
-      throw new Error('Socket not ready')
+    constructor(window) {
+        this.window = window;
+        this.topic = 'chat';
+        this.joinEvent = '/join';
+        this.message = new Subject();
+        this.messages = [];
+        this.messagesLimit = 1000;
     }
-    try {
-      this.socket.emitEvent(event, data)
-    } catch (err) {
-      onError(err)
+    connect(address = this.window.location.origin.replace(/^http/, 'ws')) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // connect to parameters
+            yield new Promise((resolve) => {
+                this.socket = new UWebSocket(address);
+                this.socket.onopen = resolve;
+            });
+            if (!this.socket) {
+                return;
+            }
+            // handshake with topic on server
+            this.socket.emitEvent(this.joinEvent, this.topic);
+            // on every message emit message
+            this.socket.on('*', (message) => {
+                if (!this.id) {
+                    this.id = message.id;
+                }
+                this.messages.push(message);
+                this.messages.splice(-this.messagesLimit);
+                this.message.next(message);
+            });
+            this.socket.onmessage = console.log.bind(console);
+        });
     }
-  }
-  initialize() {
-    return __awaiter(this, void 0, void 0, function* () {
-      // connect to parameters
-      yield this.connect()
-      if (!this.socket) {
-        return
-      }
-      // handshake with topic on server
-      this.socket.emitEvent(this.joinEvent, this.topic)
-      // on every message emit message
-      this.socket.on('*', (message) => {
-        if (!this.id) {
-          this.id = message.id
-        }
-        this.messages.push(message)
-        this.messages.splice(-this.messagesLimit)
-        this.message.next(message)
-      })
-      this.socket.onmessage = console.log.bind(console)
-    })
-  }
-  connect() {
-    return __awaiter(this, void 0, void 0, function* () {
-      return new Promise((resolve) => {
-        this.socket = new UWebSocket(this.serverUrl)
-        this.socket.onopen = resolve
-      })
-    })
-  }
-}
-ChatService = __decorate(
-  [
+};
+ChatService = __decorate([
     Injectable({
-      providedIn: 'root'
+        providedIn: 'root'
     }),
     __param(0, Inject('Window')),
-    __metadata('design:paramtypes', [Window])
-  ],
-  ChatService
-)
-export { ChatService }
+    __metadata("design:paramtypes", [Window])
+], ChatService);
+export { ChatService };
 //# sourceMappingURL=chat.service.js.map
